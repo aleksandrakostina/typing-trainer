fb51720378a0d6314368c8593b6ca1e2bc0ff89d
aleksandrakostina <kostinaalexandraa@yandex.by>
2021-05-14 13:12:59 +0300

    add redux

---
+++ src/redux/store.js
@@ -0,0 +1,12 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+const reducers = combineReducers({
+  
+});
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducers, composeEnhancer(applyMiddleware(thunk)));
+
+export default store;
